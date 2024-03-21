781c739c2aba686465514891ee294276cb916711
onnymm <phr2807@gmail.com>
2024-03-20 18:00:43 -0700

    Progreso al módulo 4

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,45 @@
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import './index.css';
 
-// const app = <img src='/react.svg' alt="" />
-// const container = document.getElementById('root')
+const USERS = [
+	{
+		name: 'Pablo Castellanos',
+		active: 'Activo',
+		role: 'Profesor'
+	},
+	{
+		name: 'José Miguel Fernández',
+		active: 'Activo',
+		role: 'Profesor'
+	}
+];
 
-// ReactDOM.createRoot(app, container)
+const Title = ({ children }) => <h1>{children}</h1>;
 
-const App = () => <img src='/react.svg' alt='' />;
+const User = ({ name, active, role }) => (
+	<div className='user'>
+		<span className='name'>{name}</span>
+		<span className='active'>{active}</span>
+		<span className='role'>{role}</span>
+	</div>
+);
+
+const List = ({ users, children }) => {
+	const usersRendered = users.map(user => <User key={user.name} {...user} />);
+	return (
+		<div className='list'>
+			{children}
+			{usersRendered}
+		</div>
+	);
+};
+
+const App = () => (
+	<List users={USERS}>
+		<Title>Listado de usuarios</Title>
+	</List>
+);
 
 const container = document.getElementById('root');
-
-ReactDOM.createRoot(container).render(<App />);
+const root = ReactDOMClient.createRoot(container);
+root.render(<App />);
