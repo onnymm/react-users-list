eb1783451fd45a9643785ceb389fd0720d3ba7e1
onnymm <phr2807@gmail.com>
2024-04-25 17:51:51 -0700

    Changes on eslint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -33,6 +33,7 @@ module.exports = {
 	plugins: ['react'],
 	rules: {
 		'react/react-in-jsx-scope': 'off',
-		'react/prop-types': 'off'
+		'react/prop-types': 'off',
+		'react/display-name': 'off'
 	}
 };
